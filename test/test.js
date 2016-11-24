1bf39e98ba9b09e77639cc40e8f8448b27c11f1c
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-24 12:49:22 +0100

    indexOf instead of includes

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 
 describe('Array', () => {
   it('should return -1 when the value is not present', () => {
-    assert.equal(false, [1, 2, 3].indexOf(4));
+    assert.equal(-1, [1, 2, 3].indexOf(4));
   });
 });
