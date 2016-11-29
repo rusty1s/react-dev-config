5e1cb159667fd964f4809a1d079d5e5b940cdfac
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-29 20:11:46 +0100

    node 4 compatibility

--- utils/spawn.js
+++ utils/spawn.js
@@ -1,6 +1,6 @@
 const crossSpawn = require('cross-spawn');
 
-module.exports = function spawn(program, config = []) {
+module.exports = function spawn(program, config) {
   const result = crossSpawn.sync('node', [
     `${process.cwd()}/node_modules/.bin/${program}`,
   ].concat(config),
