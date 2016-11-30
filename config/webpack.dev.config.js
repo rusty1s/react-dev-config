3aff2f372c0eb426f76da8831c0d8b0d1aed0cde
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-30 16:41:58 +0100

    removed irre whitespace

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -56,8 +56,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development'),
+        NODE_ENV: JSON.stringify('development'),
       },
-    })
+    }),
   ],
 };
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
     // Ensure the builds are consistent if source hasn't changed.
