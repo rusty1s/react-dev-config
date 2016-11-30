d2f76c348d7bfb4c8934d0371fead59f3edbfe15
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-30 13:35:48 +0100

    check for undefined config

--- utils/spawn.js
+++ utils/spawn.js
@@ -3,7 +3,7 @@ const crossSpawn = require('cross-spawn');
 module.exports = function spawn(program, config) {
   const result = crossSpawn.sync('node', [
     `${process.cwd()}/node_modules/.bin/${program}`,
-  ].concat(config),
+  ].concat(config || []),
   { stdio: 'inherit' });
 
   return result;
