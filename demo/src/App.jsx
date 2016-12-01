954c9c28402befc9d4cb3f7208bb7eaea77b74da
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-01 11:16:33 +0100

    added enzyme and wrote first test

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import Title from './components/Title';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 import styles from './App.css';
 
+    // <img src={logo} alt="logo" className={styles.logo} />
 const App = () => (
   <div>
     <Title>Hello, World!</Title>
-    <img src={logo} alt="logo" className={styles.logo} />
   </div>
 );
 
--- demo/src/App.test.jsx
+++ demo/src/App.test.jsx
@@ -1,3 +1,11 @@
-test('bla', () => {
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+import Title from './components/Title';
+
+test('renders without crashing', () => {
+  const app = shallow(<App />);
+  expect(app.find(Title)).toHaveLength(1);
   expect(2).toBe(2);
 });
