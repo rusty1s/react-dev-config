bde6b538263e875bacdb0d33538bcd582ef73a54
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-01 18:26:56 +0100

    typo

--- scripts/test.js
+++ scripts/test.js
@@ -7,7 +7,7 @@ const configPath = require('../utils/structure').configPath;
 // Proxy is enabled in Node.js v6.* by default; if you are not on Node v6.* 
 // yet, make sure you invoke Jest using node --harmony_proxies
 // https://facebook.github.io/jest/docs/tutorial-webpack.html
-const nodeConfig = version < 6 ? [ '--harmony_proxies' ] || [];
+const nodeConfig = nodeVersion < 6 ? [ '--harmony_proxies' ] || [];
 
 const result = spawn('jest', [
   '--config', `${configPath}/jest.config.json`,
