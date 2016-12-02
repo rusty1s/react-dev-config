c3f2f6bc37d5222cbe0622a066c808ad08ba20cd
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-02 16:33:12 +0100

    added comments

--- config/babelrc.js
+++ config/babelrc.js
@@ -4,9 +4,12 @@ module.exports = {
     'react',
   ],
   plugins: [
+    // Helpers for common functions such as `_extend` should be added only
+    // once.
     'transform-runtime',
   ],
   env: {
+    // Make ES2015 imports readable for jest.
     test: {
       plugins: ['transform-es2015-modules-commonjs'],
     },
--- config/eslintrc.js
+++ config/eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   extends: 'airbnb',
   rules: {
+    // Allow importing dev dependencies only in config or test files.
     'import/no-extraneous-dependencies': [
       2,
       { devDependencies: ['**/*.config.js', '**/*.test.js', '**/*.spec.js'] },
     ],
+    // Allow dynamic requires at runtime.
     'import/no-dynamic-require': 0,
+    // Allow assigning new properties of objects passed as arguments.
     'no-param-reassign': [
       2,
       { props: false },
