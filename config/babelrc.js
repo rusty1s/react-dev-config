9a3c53e71a1606b8c4bc94d299401e6b35e15e87
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-13 13:49:33 +0100

    removed stag 2, added rest spread transform instead

--- config/babelrc.js
+++ config/babelrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   presets: [
     ['es2015', { modules: false }],
-    'stage-2',
     'react',
   ],
   plugins: [
     // Helper for common functions such as `_extend` should be added only once.
     'transform-runtime',
+    // Transform rest properties for object destructuring assignment and spread
+    // properties for object literals.
+    // let { c, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+    // let n = { x, y, ...z };
+    'transform-object-rest-spread',
   ],
   env: {
     // Make ES2015 imports readable for jest.
