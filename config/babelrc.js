e41e6b79e15c8cb4298ac8e17eb6231f1b48129b
rusty1s <matthias.fey@tu-dortmund.de>
2016-12-14 12:12:34 +0100

    added two babel plugins for static properties and the :: bind operator

--- config/babelrc.js
+++ config/babelrc.js
@@ -5,12 +5,25 @@ module.exports = {
   ],
   plugins: [
     // Helper for common functions such as `_extend` should be added only once.
+    // https://babeljs.io/docs/plugins/transform-runtime/
     'transform-runtime',
+
     // Transform rest properties for object destructuring assignment and spread
     // properties for object literals.
     // let { c, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
     // let n = { x, y, ...z };
+    // https://babeljs.io/docs/plugins/transform-object-rest-spread/
     'transform-object-rest-spread',
+
+    // Transforms es2015 static properties as well as properties declared with
+    // the es2016 property initializer syntax.
+    // https://babeljs.io/docs/plugins/transform-class-properties/
+    'transform-class-properties',
+
+    // Compile the new function bind operator `::` to ES5
+    // obj::func is equivalent to func.bind(obj).
+    // https://babeljs.io/docs/plugins/transform-funtion-bind/
+    'transform-function-bind',
   ],
   env: {
     // Make ES2015 imports readable for jest.
