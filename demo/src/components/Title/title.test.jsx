6deb8f463b60c2f4f216cd49c81fa2d0ea3d55f5
rusty1s <matthias.fey@tu-dortmund.de>
2017-01-27 21:11:14 +0100

    corrected test

--- demo/src/components/Title/title.test.jsx
+++ demo/src/components/Title/title.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 
 import Title from './index';
 
 test('renders without crashing', () => {
-  const title = shallow(<Title>Dummy text</Title>);
+  const title = shallow(<Title text="Dummy text" />);
 
   expect(title.find('h1')).toHaveLength(1);
   expect(title.hasClass('main')).toBeTruthy();
@@ -15,19 +14,4 @@ test('renders without crashing', () => {
 test('excepts only one prop type', () => {
   expect(Object.keys(Title.propTypes)).toHaveLength(1);
   expect(Title.propTypes.children).toBeDefined();
-});
-
-test('excepts only strings passed as children', () => {
-  const stub = sinon.stub(console, 'error');
-
-  shallow(<Title><span>Dummy text</span></Title>);
-  expect(stub.calledOnce).toBeTruthy();
-
-  shallow(<Title>Dummy<hr />text</Title>);
-  expect(stub.calledTwice).toBeTruthy();
-
-  shallow(<Title>Dummy text</Title>);
-  expect(stub.calledTwice).toBeTruthy();
-
-  stub.restore();
 });
