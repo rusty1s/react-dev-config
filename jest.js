47ac02048f315b92b5cf823ff3d2c8d113392b0d
rusty1s <matthias.fey@tu-dortmund.de>
2017-08-16 13:07:34 +0200

    fixed tests with symlinked package

--- jest.js
+++ jest.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+let transform = path.join(process.cwd(), 'node_modules', 'jest-css-modules');
+if (fs.existsSync(transform)) {
+  transform = '<rootDir>/node_modules/jest-css-modules';
+}
+else {
+  transform = '<rootDir>/node_modules/react-dev-config/node_modules/jest-css-modules';
+}
+
 module.exports = {
   collectCoverage: true,
   transform: {
-    '.*': '<rootDir>/node_modules/jest-css-modules',
+    '.*': transform,
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/node_modules/react-dev-config/__mocks__/files.js',
